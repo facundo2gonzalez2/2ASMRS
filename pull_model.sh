40565e8c76c0268ad0810d91c9e5c5448f231e4a
facundo2gonzalez2 <facundo2gonzalez2@gmail.com>
2026-03-17 12:23:32 -0300

    pull latest models

---
+++ pull_model.sh
@@ -0,0 +1,14 @@
+# Pull base model
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/base_model base_model_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/base_model
+
+# Pull instruments from checkpoint models
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/instruments_from_checkpoint bass_from_checkpoint_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/instruments_from_checkpoint
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/instruments_from_checkpoint guitar_from_checkpoint_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/instruments_from_checkpoint
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/instruments_from_checkpoint voice_from_checkpoint_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/instruments_from_checkpoint
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/instruments_from_checkpoint piano_from_checkpoint_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/instruments_from_checkpoint
+
+# Pull instruments from scratch models
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/instruments_from_scratch piano_from_scratch_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/instruments_from_scratch
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/instruments_from_scratch guitar_from_scratch_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/instruments_from_scratch
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/instruments_from_scratch voice_from_scratch_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/instruments_from_scratch
+ssh venus "tar czf - -C /home/fgbarnator/2ASMRS/model/inference_models/instruments_from_scratch bass_from_scratch_no_beta" | tar xzvf - -C /home/facu/Documents/2ASMRS/model/inference_models/instruments_from_scratch
